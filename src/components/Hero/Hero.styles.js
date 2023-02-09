55786322a341cc1956f66142ab0ecd4b9c039661
Olutomisin Oluwajuwon <jolutomisin@gmail.com>
2023-02-09 23:16:35 +0100

    change animation duration for header

--- src/components/Hero/Hero.styles.js
+++ src/components/Hero/Hero.styles.js
@@ -65,7 +65,7 @@ export const HeroHeader = styled.h1`
     background-clip: text;
     background-image: linear-gradient( 45deg, #ffc5ad, #fb9a69, #F5C2A9, #E87435 );
     background-size: 100% 400%;
-    animation: ${flow} infinite 14s linear alternate;
+    animation: ${flow} infinite 15s linear alternate;
 
     @media screen and (max-width: 425px) {
         font-size: 3rem;
